8680f44f3b0398be06b55c70a58c0bbaa170ba10
benkotch <benkotch@mac.com>
2025-12-26 14:04:28 -0800

    check if it is working

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function(req, res) {
-  res.write("on the way to being a raccoon");
-  res.end();
-}
-).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.write("on the way to being 2 raccoon");
+    res.end();
+  })
+  .listen(3000);
 
-
-console.log('server started on port 3000');
+console.log("server started on port 3000");
